0825c2c0b5ac1a5a269e7cb2eb68c61670cf6bf3
Alissonfar <alissonfariascamargo@gmail.com>
2024-07-11 16:22:16 -0300

    iniciado construção do html e associado primeiros botões no JS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let nomeUsuario = window.document.getElementById('input-nome-usuario')
+
+let nomeProduto = window.document.getElementById('input-nome-produto')
+
+let valorProduto = window.document.getElementById('input-valor-produto')
+let valorProdutoValue = valorProduto.value
+
+
+
 class Itens {
     constructor(nome, valor) {
       this.nome = nome
@@ -36,10 +45,20 @@ class Itens {
     }
   }
   
-  let alisson = new Usuario('alisson')
-  
-  
-  alisson.adicionarItem('HD', 250)
-  
-  console.log(alisson)
-  
+
+  function criarUsuario() {
+    let nomeUsuarioValue = nomeUsuario.value
+
+    let usuario = new Usuario(nomeUsuarioValue)
+
+    console.log(usuario)
+    
+  }
+
+  function adicionarItem(){
+    let nomeProdutoValue = nomeProduto.value
+
+    usuario.nomeItem = nomeProdutoValue
+
+
+  }
